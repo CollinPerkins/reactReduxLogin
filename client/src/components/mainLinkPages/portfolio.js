4e0eef3a4bfd2334b746ec9c7443458e58e5adf9
Collin Perkins <collinperkinsweb@gmail.com>
2017-01-10 22:18:41 -0600

    portfolio test

--- client/src/components/mainLinkPages/portfolio.js
+++ client/src/components/mainLinkPages/portfolio.js
@@ -5,7 +5,7 @@ export default class Portfolio extends Component {
   render() {
     return (
       <div>
-        hello
+        My Portfolio
       </div>
     );
   }
--- client/src/components/mainLinkPages/projectList/projectList.js
+++ client/src/components/mainLinkPages/projectList/projectList.js
@@ -2,58 +2,67 @@ export const projectList = [
   {
     name: 'Gorillaz Tribute Page',
     html: true,
-    css: true
+    css: true,
+    link: ""
   },
   {
     name: 'Random Quote Machine',
     react: true,
     scss: true,
     axios: true,
-    bootstrap: true
+    bootstrap: true,
+    link: ""
   },
   {
     name: 'Local Weather App',
     react: true,
     scss: true,
     axios: true,
-    bootstrap: true
+    bootstrap: true,
+    link: ""
   },
   {
     name: 'Wikipedia Viewer',
     react: true,
     scss: true,
     axios: true,
-    bootstrap: true
+    bootstrap: true,
+    link: ""
   },
   {
     name: 'Twitch.tv JSON API',
     react: true,
     scss: true,
     axios: true,
-    bootstrap: true
+    bootstrap: true,
+    link: ""
   },
   {
     name: 'JavaScript Calculator',
     react: true,
     scss: true,
-    bootstrap: true
+    bootstrap: true,
+    link: ""
   },
   {
     name: 'Pomodoro Clock',
     react: true,
     scss: true,
-    bootstrap: true
+    bootstrap: true,
+    link: ""
   },
   {
     name: 'Tic Tac Toe Game',
     react: true,
     scss: true,
-    bootstrap: true
+    bootstrap: true,
+    link: ""
   },
   {
     name: 'Simon Game',
     react: true,
     scss: true,
-    bootstrap: true
+    bootstrap: true,
+    link: ""
   }
 ];
